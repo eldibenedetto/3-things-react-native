f69a5f2101a3774f91dca6f326215e5d4a7280d8
github username <github email address>
2018-02-14 17:36:54 -0500

    added navigation

--- App.js
+++ App.js
@@ -1,69 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { StackNavigator } from 'react-navigation'
 import TaskEdit from './screens/TaskEdit'
+import Profile from './screens/Profile'
 
-export default class App extends React.Component {
-
-  state = {
-    firstInt: 255,
-    secondInt: 0,
-    thirdInt: 0
-  }
-
-  componentDidMount() {
-    this.repeat = setInterval(() => this.color(), 50)
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.repeat)
-  }
-
-  color = () => {
-    let { firstInt, secondInt, thirdInt } = this.state
-    if (firstInt === 255 && secondInt !== 255 && thirdInt === 0) {
-      this.setState({secondInt: secondInt += 5})
-    } else if (firstInt !== 0 && secondInt === 255 && thirdInt === 0) {
-      this.setState({firstInt: firstInt -= 5})
-    } else if (firstInt === 0 && secondInt === 255 && thirdInt !== 255) {
-      this.setState({thirdInt: thirdInt += 5})
-    } else if (firstInt === 0 && secondInt !== 0 && thirdInt === 255) {
-      this.setState({secondInt: secondInt -= 5})
-    } else if (firstInt !== 255 && secondInt === 0 && thirdInt === 255) {
-      this.setState({firstInt: firstInt += 5})
-    } else if (firstInt === 255 && secondInt === 0 && thirdInt !== 0) {
-      this.setState({thirdInt: thirdInt -= 5})
-    }
-  }
-
+class App extends React.Component {
 
   render() {
-    let { firstInt, secondInt, thirdInt } = this.state
-    let currentColor = `rgb(${firstInt}, ${secondInt}, ${thirdInt})`
-
     return (
-      <View style={[styles.container, { backgroundColor: currentColor}]}>
-        <Text style={styles.text}>My frist mobile app!</Text>
-        <TaskEdit />
-      </View>
+      <RootStack />
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'space-around'
+export default StackNavigator({
+  TaskEdit: {
+    screen: TaskEdit
   },
-  text: {
-    color: 'black',
-    fontSize: 24,
-    fontWeight: '500',
-    borderWidth: 0.5,
-    borderColor: 'black'
-  },
-  textInput: {
-    flex: 1,
-    margin: 5
-  }
+  Profile: { screen: Profile }
 })
